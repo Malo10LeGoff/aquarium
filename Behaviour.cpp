c4c3ad0c90a860ef9845c9b1f5525b170f5bc4c3
Minh Tri TRUONG <ngoc-minh-tri.truong@imt-atlantique.net>
2022-03-07 00:57:25 +0100

    partial implem of MultipleBehaviour, need to add the policy function

--- Behaviour.cpp
+++ Behaviour.cpp
@@ -1,6 +1,12 @@
 #include <Behaviour.h>
+#include <cmath>
+// GregariousBehaviour
 
-std::array<float,2> GregariousBehaviour::moveDirection(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const {
+GregariousBehaviour::GregariousBehaviour() {
+
+};
+std::array<float,2> GregariousBehaviour::moveDirection(const std::array<float,2> creatureCoordinates,
+                                                       const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const {
     float cumX = 0;
     float cumY = 0;
     for (auto it = std::begin(visibleCreatures); it != std::end(visibleCreatures); ++it){
@@ -13,3 +19,96 @@ std::array<float,2> GregariousBehaviour::moveDirection(const std::vector<std::ar
     cumY /= visibleCreatures.size();
     return std::array<float,2>{cumX, cumY};
 };
+
+
+// FearfulBehaviour
+FearfulBehaviour::FearfulBehaviour() {
+    moveSpeedMultiplier_ = 1.1;
+    maxNeighbours_ = 10;
+};
+
+FearfulBehaviour::FearfulBehaviour(int maxNeighbours, float moveSpeedMultiplier) {
+    moveSpeedMultiplier_ = moveSpeedMultiplier;
+    maxNeighbours_ = maxNeighbours;
+};
+
+/**
+ * If there's too many people detected, then run in opposite of the mean directions
+ * the other creatures are in.
+ * Will return [0,0] if there's no change of direction planned
+ */
+std::array<float, 2> FearfulBehaviour::moveDirection(const std::array<float,2> creatureCoordinates,
+                                                     const std::vector<std::array<std::array < float, 2>, 2>> visibleCreatures) const {
+    float x = 0;
+    float y = 0;
+    if (visibleCreatures.size() > (unsigned)this->maxNeighbours_) {
+        // Calculate the running direction
+        for (auto it= std::begin(visibleCreatures); it != std::end(visibleCreatures); ++it) {
+            // *it is [moveDirection , coordinates]
+            x+= (*it)[1][0] - creatureCoordinates[0];
+            y+= (*it)[1][1] - creatureCoordinates[1];
+        };
+        x /= visibleCreatures.size();
+        y /= visibleCreatures.size();
+    }
+    return std::array<float,2>{x, y};
+};
+
+float FearfulBehaviour::moveSpeedMultiplier(const std::vector<std::array<std::array < float, 2>, 2>> visibleCreatures) {
+    if (visibleCreatures.size() > (unsigned)this->maxNeighbours_) {
+        return this->moveSpeedMultiplier_;
+    }
+    else {
+        return 1;
+    }
+}
+
+// KamikazeBehaviour
+KamikazeBehaviour::KamikazeBehaviour(){
+
+};
+
+int min_element(const std::vector<float> arr) {
+    int index =0;
+    for (int i=0; (unsigned)i< arr.size(); ++i) {
+        if (arr[i] < arr[index] ) {
+            index = i;
+        }
+    }
+    return index;
+}
+
+/**
+ * Run into the closest creature to the current creature
+ * @param creatureCoordinates
+ * @return [x,y] representing the direction to move in
+ */
+std::array<float, 2> KamikazeBehaviour::moveDirection(const std::array<float, 2> creatureCoordinates,
+                                                      const std::vector<std::array<std::array < float, 2>, 2>> visibleCreatures) const {
+    // calculate the neighbours' distances
+    // we calculate the squared dxistnace for performance reasons
+    std::vector<float> neighbourDistances;
+    for (auto it= std::begin(visibleCreatures); it != std::end(visibleCreatures); ++it) {
+        // *it is [moveDirection , coordinates]
+        float d = std::pow((*it)[1][0] - creatureCoordinates[0], 2) + std::pow((*it)[1][1] - creatureCoordinates[1], 2);
+        neighbourDistances.push_back(d);
+    };
+    int closest_neighbour_idx = min_element(neighbourDistances);
+    return std::array<float,2> { visibleCreatures[closest_neighbour_idx][1][0] - creatureCoordinates[0],
+                                 visibleCreatures[closest_neighbour_idx][1][1] - creatureCoordinates[1]};
+
+};
+
+
+// MultipleBehaviours
+MultipleBehaviours::MultipleBehaviours() {
+    behaviours_ = std::vector<std::unique_ptr<InterfaceBehaviour>>{};
+}
+
+void MultipleBehaviours::add(std::unique_ptr<InterfaceBehaviour>& behaviour) {
+    this->behaviours_.push_back(std::move(behaviour));
+}
+
+float MultipleBehaviours::moveSpeedMultiplier(const std::vector<std::array<std::array < float, 2>, 2>> visibleCreatures) const {
+    return 1;
+}
--- Behaviour.h
+++ Behaviour.h
@@ -4,7 +4,7 @@
 #define _BEHAVIOUR_H_
 #include <array>
 #include <vector>
-
+#include <memory>
 class InterfaceBehaviour {
 public:
     /**
@@ -14,33 +14,50 @@ public:
      * @return moveDirection
      *          std::array<float,2> The direction the creature wants to move.
      */
-    virtual std::array<float,2> moveDirection(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const = 0;
+    virtual std::array<float,2> moveDirection(const std::array<float,2> creatureCoordinates, const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const = 0;
     virtual float moveSpeedMultiplier(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const {
         return 1;
     };
+    virtual std::array<float,3> policy(const std::array<float,2> creatureCoordinates, const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const =0;
 
 };
 
 class GregariousBehaviour: public InterfaceBehaviour {
 public:
-    std::array<float,2> moveDirection(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
+    GregariousBehaviour();
+    std::array<float,2> moveDirection(const std::array<float,2> creatureCoordinates, const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
 };
 
 class FearfulBehaviour: public InterfaceBehaviour {
 public:
-    std::array<float,2> moveDirection(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
-    float moveSpeedMultiplier();
+    FearfulBehaviour();
+    FearfulBehaviour(int maxNeighbours, float moveSpeedMultiplier);
+    std::array<float,2> moveDirection(const std::array<float,2> creatureCoordinates,const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
+    float moveSpeedMultiplier(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures);
 private:
     float moveSpeedMultiplier_;
+    int maxNeighbours_;
 };
 
 class KamikazeBehaviour: public InterfaceBehaviour {
 public:
-    std::array<float,2> moveDirection(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
+    KamikazeBehaviour();
+    std::array<float,2> moveDirection(const std::array<float,2> creatureCoordinates, const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
 };
 
 class PlanningBehaviour:public InterfaceBehaviour {
 public:
-    std::array<float,2> moveDirection(const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
+    std::array<float,2> moveDirection(const std::array<float,2> creatureCoordinates ,const std::vector<std::array<std::array<float,2>,2>> visibleCreatures) const override;
+};
+
+class MultipleBehaviours: public InterfaceBehaviour {
+public:
+    MultipleBehaviours();
+    std::array<float, 2> moveDirection(const std::array<float, 2> creatureCoordinates, const std::vector<std::array<std::array<float, 2>, 2>> visibleCreatures) const override;
+    float moveSpeedMultiplier(const std::vector<std::array<std::array<float, 2>, 2>> visibleCreatures) const override;
+    void add(std::unique_ptr<InterfaceBehaviour>& behaviour);
+    void remove(std::unique_ptr<InterfaceBehaviour>& behaviour);
+private:
+    std::vector<std::unique_ptr<InterfaceBehaviour>> behaviours_;
 };
 #endif //_BEHAVIOUR_H_
