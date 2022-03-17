1dbe7f6f496acc146794a1077cad5b27ede61076
Minh Tri TRUONG <ngoc-minh-tri.truong@imt-atlantique.net>
2022-03-17 19:26:57 +0100

    Added fixtures, better tests for GreagriousBehaviour

--- test/behaviourTest.cpp
+++ test/behaviourTest.cpp
@@ -16,24 +16,31 @@ protected:
     std::array<float,2> coordinates {0,0};
 };
 
-TEST(TestGregariousBehaviour, TestInit) {
-    auto ret = b.moveDirection(std::array<float,2>{0,0} , arr);
+TEST_F(TestGregariousBehaviour, TestInit) {
+    auto ret = b.moveDirection(std::array<float,2>{0,0} , visibleCreatures);
     // check for colinearity
-    EXPECT_EQ(0, arr[0][0][0] * ret[1] - arr[0][0][1] * ret[0]);
+    EXPECT_EQ(0, visibleCreatures[0][0][0] * ret[1] - visibleCreatures[0][0][1] * ret[0]);
     std::cout << "direction followed\n" << "x:" << ret[0] <<"\ny:" <<  ret[1] << "\n";
+
 }
 
-TEST(TestGregariousBehaviour, TestFollowsAverage) {
-    arr.push_back(std::array<std::array<float,2>,2>{{{2 , -1}, {0,0} }});
-    std::array<float,2> expectedDirection {3,0};
-    auto ret = b.moveDirection(std::array<float,2>{0,0} , arr);
+TEST_F(TestGregariousBehaviour, TestFollowsAverage) {
+    visibleCreatures.push_back(std::array<std::array<float,2>,2>{{{2 , -1}, {0,0} }});
+std::array<float,2> expectedDirection {0,0};
+    //std::cout << "visibleCreatures size " << visibleCreatures.size() << "\n";
+    for (auto it = std::begin(visibleCreatures); it != std::end(visibleCreatures); ++it) {
+        //std::cout << " x_move " << (*it)[0][0] << " y_move " << (*it)[0][1] << "\n";
+        expectedDirection[0] += (*it)[0][0];
+        expectedDirection[1] += (*it)[0][1];
+    }
+    auto ret = b.moveDirection(std::array<float,2>{0,0} , visibleCreatures);
     // check for colinearity
     EXPECT_EQ(0, expectedDirection[0] * ret[1] - expectedDirection[1] *ret[0] );
-    std::cout << "direction followed\n" << "x:" << ret[0] <<"\ny:" <<  ret[1] << "\n";
+    std::cout << "direction followed :\n" << "x:" << ret[0] <<"\ny:" <<  ret[1] << "\n";
 }
 
-TEST(TestGregariousBehaviour, TestSpeedCoef) {
-    EXPECT_EQ(1, b.moveSpeedMultiplier(arr));
+TEST_F(TestGregariousBehaviour, TestSpeedCoef) {
+    EXPECT_EQ(1, b.moveSpeedMultiplier(visibleCreatures));
 }
 
 // FearfulBehaviour
@@ -49,15 +56,14 @@ protected:
     FearfulBehaviour b_default = FearfulBehaviour();
     std::array<float,2> coordinates {0,0};
 };
-TEST(TestFearfulBehaviour, TestDefaultInit) {
+TEST_F(TestFearfulBehaviour, TestDefaultInit) {
     // Speed coef should be 1 if there are not too many creatures around
     for (int i = 0; i<100; ++i){
-        float moveSpeedMultiplier = b_default.moveSpeedMultiplier(visibleCreatures)
-        if (std::size(arr) > (unsigned)b_default.maxNeighbours() ){
-            EXPECT_GT(moveSpeedMultiplier , 1)
-        }
-        else {
-            EXPECT_EQ(moveSpeedMultiplier, 1)
+        float moveSpeedMultiplier = b_default.moveSpeedMultiplier(visibleCreatures);
+        if (visibleCreatures.size() > (unsigned)b_default.maxNeighbours() ){
+            EXPECT_GT(moveSpeedMultiplier , 1);
+        } else {
+            EXPECT_EQ(moveSpeedMultiplier, 1);
         }
     }
 
