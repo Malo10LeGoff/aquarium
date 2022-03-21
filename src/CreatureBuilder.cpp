97fa62a99cbc096795d781d38e4207a1ab99383a
Minh Tri <minhtri3007@hotmail.com>
2022-03-21 03:04:51 +0100

    start writing CreatureBuilderClass

--- src/CreatureBuilder.cpp
+++ src/CreatureBuilder.cpp
@@ -3,3 +3,96 @@
 //
 
 #include "CreatureBuilder.h"
+#include <random>
+
+std::unique_ptr<Creature> CreatureBuilder::make(int creatureID) {
+    builder.reset();
+    builder.initID(next_++);
+    builder.initAccessories();
+    builder.initSensors();
+    builder.initBehaviour();
+    builder.initPosition();
+    builder.initPosition();
+    builder.initVitesse();
+    return builder.getResult();
+}
+
+RandomBuilder::RandomBuilder(CreatureBuilder &t_director, double t_eyeProb, double t_earProb, double t_shellProb, double t_camoProb,
+                             double t_finProb, std::array<double, 4> t_behaviourDistrib): director(t_director) ,m_creature(&director.milieu) {
+    m_eyeProb = t_eyeProb;
+    m_earProb = t_earProb;
+    m_shellProb = t_shellProb;
+    m_camoProb = t_camoProb;
+    m_finProb = t_finProb;
+
+    m_behaviourDistrib = t_behaviourDistrib;
+    //  Normalize the distrib ( just case)
+    normalizeDistrib();
+
+}
+
+RandomBuilder::RandomBuilder(CreatureBuilder &t_director):director(t_director), m_creature(&director.milieu) {
+
+}
+
+void RandomBuilder::initID(int t_id) {
+    m_creature.id = t_id;
+
+}
+
+void RandomBuilder::initAccessories() {
+    // draw accessory type
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    // Shell
+    std::bernoulli_distribution hasShell (m_shellProb);
+    if (hasShell(mt)){
+        std::unique_ptr<InterfaceAccessory>(new Shell)
+    }
+
+}
+
+void RandomBuilder::initSensors() {
+
+}
+
+void RandomBuilder::initBehaviour() {
+
+}
+
+void RandomBuilder::initPosition() {
+
+}
+
+void RandomBuilder::initVitesse() {
+
+}
+
+std::array<double,4> RandomBuilder::getCumDistrib() {
+    std::array<double, 4> cumDistrib {0,0,0,0};
+    double cumSum = 0;
+    for (unsigned int i =0; i < m_behaviourDistrib.size(); ++i){
+        cumSum+= m_behaviourDistrib[i];
+        m_behaviourDistrib[i] = cumSum;
+    }
+    return cumDistrib;
+}
+
+void RandomBuilder::normalizeDistrib() {
+    int s = 0;
+    for (auto nb: m_behaviourDistrib) {
+        s+= nb;
+    }
+    for (unsigned int i = 0; i< m_behaviourDistrib.size(); ++i) {
+        m_behaviourDistrib[i] /= s;
+    }
+}
+
+std::unique_ptr<Creature> RandomBuilder::getResult() {
+    return std::unique_ptr<Creature>();
+}
+
+void RandomBuilder::reset() {
+
+}
+
--- src/CreatureBuilder.h
+++ src/CreatureBuilder.h
@@ -4,10 +4,61 @@
 
 #ifndef AQUARIUM_CREATUREBUILDER_H
 #define AQUARIUM_CREATUREBUILDER_H
+#include <memory>
+#include "Creature.h"
+
+class CreatureBuilder;
+
+class BuilderInterface {
+public :
+    virtual void initID(int id) = 0;
+    virtual void initAccessories() = 0;
+    virtual void initSensors()= 0;
+    virtual void initBehaviour() = 0;
+    virtual void initPosition() = 0;
+    virtual void initVitesse() = 0;
+    virtual std::unique_ptr<Creature> getResult() = 0;
+    virtual void reset() = 0;
+};
+
+class RandomBuilder : public BuilderInterface {
+public:
+    RandomBuilder(CreatureBuilder &t_director,double t_eyeProb, double t_earProb, double t_shellProb, double t_camoProb, double t_finProb, std::array<double, 4> t_behaviourDistrib);
+    RandomBuilder(CreatureBuilder& t_director);
+    void initID(int t_id) override;
+    void initAccessories() override;
+    void initSensors() override;
+    void initBehaviour() override;
+    void initPosition() override;
+    void initVitesse() override;
+    std::unique_ptr<Creature> getResult() override;
+    void reset() override;
+private :
+    CreatureBuilder& director;
+    Creature m_creature;
+
+    // random parameters
+    double m_eyeProb = 0.5;
+    double m_earProb = 0.5;
+    double m_shellProb = 0.5;
+    double m_camoProb = 0.5;
+    double m_finProb = 0.5;
+    std::array<double, 4> m_behaviourDistrib {{0.25, 0.25, 0.25 ,0.25}};
+
+    array<double, 4> getCumDistrib();
+    void normalizeDistrib();
+};
 
 
 class CreatureBuilder {
-
+public:
+    CreatureBuilder(Milieu& t_milieu, BuilderInterface& t_builder ): milieu(t_milieu), builder(t_builder) {};
+    std::unique_ptr<Creature> make(int creatureID);
+    void setBuilder(BuilderInterface& t_builder) {builder = t_builder;};
+    Milieu& milieu;
+private:
+    BuilderInterface& builder;
+    int next_ = 0;
 };
 
 
